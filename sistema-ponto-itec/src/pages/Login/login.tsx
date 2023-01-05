8a5ecd77df5ff792220fdef5a34a098940240063
techpril <priscila.teodorio2015@gmail.com>
2023-01-05 05:26:12 -0300

    Iniciando projeto e definindo breackpoints

---
+++ sistema-ponto-itec/src/pages/Login/login.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import "./styles.css";
+
+export function Login() {
+
+    return (
+        <div className="flex w-full">
+        <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                <h1 className="titulozinho">oi</h1>
+                </a>
+            </div>
+            </nav>
+                              
+        </div>
+    )
+  
+}
